aa9a021bc45ebd7e9e6409e22d82d55a61b1b1cd
Tanvir Rahman <rahmantanvir25558@gmail.com>
2021-04-07 12:29:16 +0600

    added license info

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-module.exports.isPropertyExist = (obj, arr, options = {type: "every"}) => {
+/*!
+ * is-property-exist <https://github.com/Tanvir-rahman/is-property-exist>
+ *
+ * Copyright (c) 2021, Tanvir Rahman.
+ * Released under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function (obj, arr, options = {type: "every"}) {
 	let type = options.type;
 	if (options.type == 'some') {
   	type = "some";
